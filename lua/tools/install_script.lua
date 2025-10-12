c3794a1e2da98cbd55f26e89015981fc123962b3
Jvpanda <j.v.pando3@gmail.com>
2025-10-11 22:31:14 -0700

    Added cmake and ninja, as well as determining if the executable already exists on the device

--- lua/tools/install_script.lua
+++ lua/tools/install_script.lua
@@ -1,56 +1,77 @@
-local baseInstallDir = vim.fn.expand "~" .. "/AppData/Local/nvim-data/myInstallations/"
+--TODO create links for linux
+local baseInstallDir = vim.fn.expand "~" .. "/AppData/Local/nvim-data/PandaInstallations/"
 local Pkgs = {
     ripgrep = {
         url = "https://github.com/BurntSushi/ripgrep/releases/download/14.1.1/ripgrep-14.1.1-x86_64-pc-windows-msvc.zip",
         installDir = baseInstallDir,
         binaryDir = baseInstallDir .. "ripgrep-14.1.1-x86_64-pc-windows-msvc/",
+        executable = "rg",
     },
     lua_ls = {
         url = "https://github.com/LuaLS/lua-language-server/releases/download/3.15.0/lua-language-server-3.15.0-win32-x64.zip",
         installDir = baseInstallDir .. "lua-language-server-3.15.0-win32-x64/",
         binaryDir = baseInstallDir .. "lua-language-server-3.15.0-win32-x64/bin/",
+        executable = "lua-language-server",
     },
     stylua = {
         url = "https://github.com/JohnnyMorganz/StyLua/releases/download/v2.1.0/stylua-windows-x86_64.zip",
         installDir = baseInstallDir .. "stylua-windows-x86_64/",
         binaryDir = baseInstallDir .. "stylua-windows-x86_64/",
+        executable = "stylua",
     },
     arduino_lsp = {
         url = "https://github.com/arduino/arduino-language-server/releases/download/0.7.7/arduino-language-server_0.7.7_Windows_64bit.zip",
         installDir = baseInstallDir .. "arduino-language-server_0.7.7_Windows_64bit/",
         binaryDir = baseInstallDir .. "arduino-language-server_0.7.7_Windows_64bit/",
+        executable = "arduino-language-server",
     },
     arduino_cli = {
         url = "https://github.com/arduino/arduino-cli/releases/download/v1.3.1/arduino-cli_1.3.1_Windows_64bit.zip",
         installDir = baseInstallDir .. "arduino-cli_1.3.1_Windows_64bit",
         binaryDir = baseInstallDir .. "arduino-cli_1.3.1_Windows_64bit",
+        executable = "arduino-cli",
     },
     clangd = {
         url = "https://github.com/clangd/clangd/releases/download/21.1.0/clangd-windows-21.1.0.zip",
         installDir = baseInstallDir,
         binaryDir = baseInstallDir .. "clangd_21.1.0/bin/",
+        executable = "clangd",
     },
     raddbg = {
         url = "https://github.com/EpicGamesExt/raddebugger/releases/download/v0.9.21-alpha/raddbg.zip",
         installDir = baseInstallDir .. "raddbg/",
         binaryDir = baseInstallDir .. "raddbg/",
+        executable = "raddbg",
+    },
+    ninja = {
+        url = "https://github.com/ninja-build/ninja/releases/download/v1.13.1/ninja-win.zip",
+        installDir = baseInstallDir .. "ninja/",
+        binaryDir = baseInstallDir .. "ninja/",
+        executable = "ninja",
+    },
+    cmake = {
+        url = "https://github.com/Kitware/CMake/releases/download/v4.1.2/cmake-4.1.2-windows-x86_64.zip",
+        installDir = baseInstallDir,
+        binaryDir = baseInstallDir .. "cmake-4.1.2-windows-x86_64/bin/",
+        executable = "cmake",
     },
 }
-local fullPkgs =
-    { cpp = { Pkgs.clangd, Pkgs.raddbg }, lua = { Pkgs.lua_ls, Pkgs.stylua }, arduino = { Pkgs.arduino_cli, Pkgs.arduino_lsp }, tools = { Pkgs.ripgrep } }
 
--- vim.notify(
--- )
---
+local fullPkgs = {
+    cpp = { Pkgs.clangd, Pkgs.raddbg, Pkgs.ninja, Pkgs.cmake },
+    lua = { Pkgs.lua_ls, Pkgs.stylua },
+    arduino = { Pkgs.arduino_cli, Pkgs.arduino_lsp },
+    tools = { Pkgs.ripgrep },
+}
 
 if vim.fn.has "Win32" == 0 then
     Pkgs = {}
-    baseInstallDir = "$XDG_DATA_HOME/nvim/myInstallations/"
+    baseInstallDir = "$XDG_DATA_HOME/nvim/PandaInstallations/"
 end
 
 local createInstallDir = function()
-    if vim.fn.isdirectory(vim.fn.expand "~" .. "/AppData/Local/nvim-data/myInstallations") == 0 then
-        vim.fn.mkdir(vim.fn.expand "~" .. "/AppData/Local/nvim-data/myInstallations")
+    if vim.fn.isdirectory(baseInstallDir) == 0 then
+        vim.fn.mkdir(baseInstallDir)
     end
 end
 
@@ -107,7 +128,11 @@ end
 
 local installPkg = function(pkg)
     if vim.fn.isdirectory(pkg.binaryDir) == 1 then
-        print "Package Already Exists!"
+        print(pkg.executable .. " is already installed!")
+        return
+    end
+    if vim.fn.executable(pkg.executable) == 1 then
+        print(pkg.executable .. " is already on the device!")
         return
     end
     download_and_unzip(pkg.url, pkg.installDir)
