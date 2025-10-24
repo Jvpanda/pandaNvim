32a82e72e3c33db1979b169fc0628cd24caf8970
Jvpanda <j.v.pando3@gmail.com>
2025-10-24 12:58:27 -0700

    integrated editor configs to automatically generate in new projects

--- lua/language_configurations/cpp/cpp_build_and_run.lua
+++ lua/language_configurations/cpp/cpp_build_and_run.lua
@@ -4,21 +4,28 @@ local cpp_opts = require "language_configurations.cpp.cpp_opts"
 local workspace = require "tools.workspace_tracker"
 local raddbg = require "language_configurations.cpp.raddbg"
 
--- [[Build Environment Setup]]
+-- [[ Editor Environment Setup]]
+
 local generate_clang_format = function()
     if vim.fn.filereadable ".clang-format" == 1 then
         return
     end
 
-    local fp = vim.fn.fnamemodify(vim.fn.expand "$MYVIMRC", ":h") .. "/lua/language_configurations/cpp/.clang-format"
-    if general.isOnWindows() then
-        fp = fp:gsub("/", "\\")
-        os.execute("COPY " .. fp .. " " .. workspace.getWindowsWorkspace())
-    else
-        --NEEDS LINUX TEST
-        vim.system { "cp", fp, workspace.getWorkspace() }
-    end
+    local source = vim.fn.fnamemodify(vim.fn.expand "$MYVIMRC", ":h") .. "/lua/language_configurations/cpp/.clang-format"
+    local destination = workspace.getWorkspace()
+    general.copy_file(source, destination)
 end
+
+local generate_editor_config = function()
+    if vim.fn.filereadable ".editorconfig" == 1 then
+        return
+    end
+
+    local source = vim.fn.fnamemodify(vim.fn.expand "$MYVIMRC", ":h") .. "/lua/language_configurations/cpp/.editorconfig"
+    local destination = workspace.getWorkspace()
+    general.copy_file(source, destination)
+end
+
 -- [[Cmake Setup]]
 local generate_cmake_lists = function()
     if vim.fn.filereadable "CMakeLists.txt" == 0 then
@@ -51,15 +58,16 @@ M.create_or_switch_symlinks = function()
     if workspace.isWorkspaceSet() == false then
         return
     end
+
     --Note vim.system is async and newer, vim.fn.system is not and not newer
     if general.isOnWindows() then
-        if vim.fn.filereadable(workspace.getWorkspace() .. "compile_commands.json") == 1 then
-            vim.fn.system("del " .. workspace.getWindowsWorkspace() .. "compile_commands.json")
+        if vim.fn.filereadable(workspace.getWorkspace() .. "/build/compile_commands.json") == 1 then
+            vim.fn.system("del " .. workspace.getWindowsWorkspace() .. "build\\compile_commands.json")
         end
         vim.fn.system(
             "mklink "
                 .. workspace.getWindowsWorkspace()
-                .. "compile_commands.json "
+                .. "build\\compile_commands.json "
                 .. workspace.getWindowsWorkspace()
                 .. "build\\"
                 .. cpp_opts.buildType
@@ -67,7 +75,7 @@ M.create_or_switch_symlinks = function()
         )
     else
         if vim.fn.filereadable(workspace.getWorkspace() .. "compile_commands.json") == 1 then
-            vim.fn.system("unlink " .. workspace.getWorkspace() .. "compile_commands.json")
+            vim.fn.system("unlink " .. workspace.getWorkspace() .. "build/compile_commands.json")
         end
         vim.fn.system(
             "ln "
@@ -76,7 +84,7 @@ M.create_or_switch_symlinks = function()
                 .. cpp_opts.buildType
                 .. "/compile_commands.json "
                 .. workspace.getWorkspace()
-                .. "compile_commands.json"
+                .. "build/compile_commands.json"
         )
     end
 end
@@ -88,6 +96,7 @@ M.cmake_generate_ninja_files = function()
     end
 
     generate_clang_format()
+    generate_editor_config()
     generate_cmake_lists()
     create_cmake_build_folders()
 
@@ -104,6 +113,84 @@ M.cmake_generate_ninja_files = function()
     M.create_or_switch_symlinks()
 end
 
+-- [[Compiling and running]]
+M.cmake_compile = function()
+    if workspace.isWorkspaceSet() == false then
+        return "Please set a home directory"
+    end
+
+    if vim.fn.isdirectory("build/" .. cpp_opts.buildType) == 0 then
+        M.cmake_generate_ninja_files()
+    end
+    vim.cmd.wa()
+    print("Compiling... with build type " .. cpp_opts.buildType)
+    local filepath = workspace.getWorkspace() .. "build/" .. cpp_opts.buildType .. "/"
+    local result = vim.fn.system { "cmake", "--build", filepath }
+    return result
+end
+
+M.run_cpp = function()
+    if workspace.isWorkspaceSet() == false then
+        vim.notify "Please set a home directory"
+        return
+    end
+
+    local filepath = ""
+    if general.isOnWindows() then
+        filepath = workspace.getWorkspace() .. "build/" .. cpp_opts.buildType .. "/execBinary.exe"
+    else
+        filepath = workspace.getWorkspace() .. "build/" .. cpp_opts.buildType .. "/execBinary"
+    end
+
+    if cpp_opts.buildType == "Debug" and cpp_opts.debugRunStart ~= "No raddbg" and general.isOnWindows() == true then
+        raddbg.runRadDbg(filepath, { run = cpp_opts.debugRunStart })
+        return
+    end
+
+    local oldCommandHeight = vim.o.cmdheight
+    vim.o.cmdheight = 15
+
+    if cpp_opts.runWindow == "floatingWindow" then
+        general.create_floating_window(cpp_opts.vimFloatingWindowSize)
+        vim.cmd.terminal(filepath)
+        general.setDelWinKeymapForBuffer()
+    elseif cpp_opts.runWindow == "window" then
+        vim.cmd "vsplit"
+        vim.cmd.terminal(filepath)
+        general.setDelWinKeymapForBuffer()
+    elseif cpp_opts.runWindow == "external" then
+        vim.cmd("!start " .. filepath)
+    elseif cpp_opts.runWindow == "external_permanent" then
+        vim.cmd("!start cmd /k " .. filepath)
+    end
+
+    vim.o.cmdheight = oldCommandHeight
+end
+
+M.compile_and_run = function()
+    if workspace.isWorkspaceSet() == false then
+        vim.notify "Please set a home directory"
+        return
+    end
+
+    local compilationResult = "-------\n" .. M.cmake_compile() .. "-------\n"
+
+    if
+        string.find(compilationResult, "error") == nil
+        and string.find(compilationResult, "warning") == nil
+        and compilationResult ~= "Please set a home directory"
+    then
+        local oldCommandHeight = vim.o.cmdheight
+        vim.o.cmdheight = 15
+        print(compilationResult)
+        vim.o.cmdheight = oldCommandHeight
+        M.run_cpp()
+    else
+        vim.notify(compilationResult)
+    end
+end
+
+-- [[ Additional Functions ]]
 M.add_file_to_cmake_lists = function()
     if workspace.isWorkspaceSet() == false then
         print "Please set a workspace first"
@@ -156,77 +243,6 @@ M.add_file_to_cmake_lists = function()
         fileOut:close()
     end
     print "Cmake writen to succesfully"
-end
-
--- [[Compiling and running]]
-M.cmake_compile = function()
-    if workspace.isWorkspaceSet() == false then
-        return "Please set a home directory"
-    end
-    if vim.fn.isdirectory("build/" .. cpp_opts.buildType) == 0 then
-        M.cmake_generate_ninja_files()
-    end
-    vim.cmd.wa()
-    print("Compiling... with build type " .. cpp_opts.buildType)
-    local filepath = workspace.getWorkspace() .. "build/" .. cpp_opts.buildType .. "/"
-    local result = vim.fn.system { "cmake", "--build", filepath }
-    return result
-end
-
-M.run_cpp = function()
-    if workspace.isWorkspaceSet() == false then
-        vim.notify "Please set a home directory"
-        return
-    end
-
-    local filepath = ""
-    if general.isOnWindows() then
-        filepath = workspace.getWorkspace() .. "build/" .. cpp_opts.buildType .. "/execBinary.exe"
-    else
-        filepath = workspace.getWorkspace() .. "build/" .. cpp_opts.buildType .. "/execBinary"
-    end
-
-    if cpp_opts.buildType == "Debug" and cpp_opts.debugRunStart ~= "No raddbg" and general.isOnWindows() == true then
-        raddbg.runRadDbg(filepath, { run = cpp_opts.debugRunStart })
-        return
-    end
-
-    local oldCommandHeight = vim.o.cmdheight
-    vim.o.cmdheight = 15
-
-    if cpp_opts.runWindow == "floatingWindow" then
-        general.create_floating_window(cpp_opts.vimFloatingWindowSize)
-        vim.cmd.terminal(filepath)
-        general.setDelWinKeymapForBuffer()
-    elseif cpp_opts.runWindow == "window" then
-        vim.cmd "vsplit"
-        vim.cmd.terminal(filepath)
-        general.setDelWinKeymapForBuffer()
-    elseif cpp_opts.runWindow == "external" then
-        vim.cmd("!start " .. filepath)
-    elseif cpp_opts.runWindow == "external_permanent" then
-        vim.cmd("!start cmd /k " .. filepath)
-    end
-
-    vim.o.cmdheight = oldCommandHeight
-end
-
-M.compile_and_run = function()
-    local compilationResult = "-------\n" .. M.cmake_compile() .. "-------\n"
-
-    if
-        string.find(compilationResult, "error") == nil
-        and string.find(compilationResult, "warning") == nil
-        and compilationResult ~= "Please set a home directory"
-    then
-        local oldCommandHeight = vim.o.cmdheight
-        vim.o.cmdheight = 15
-        print(compilationResult)
-        vim.o.cmdheight = oldCommandHeight
-        M.run_cpp()
-    else
-        vim.notify(compilationResult)
-    end
 end
 
 return M
