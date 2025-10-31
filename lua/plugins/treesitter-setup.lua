c9bfac7485ae9cbc575da0014226d781d5b8412c
Jvpanda <j.v.pando3@gmail.com>
2025-10-30 18:43:58 -0700

    Changed the max context

--- lua/plugins/treesitter-setup.lua
+++ lua/plugins/treesitter-setup.lua
@@ -1,40 +1,40 @@
 return {
     { -- Highlight, edit, and navigate code
-        'nvim-treesitter/nvim-treesitter',
-        build = ':TSUpdate',
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
         -- I MADE THIS CONFIG AND IM SO DAMN PROUD. I LEARNED THE DIFFERENCE BETWEEN OPTS AND CONFIG AND HOW TO MAKE A CUSTOM CONFIG THAT SUITED MY NEEDS. YES. FUCK YOU FUCK YES.
         config = function()
-            require 'nvim-treesitter.install'.prefer_git = false
-            if vim.fn.has 'win32' == 1 then
-                require('nvim-treesitter.install').compilers = { 'clang', 'cl', 'zig' }
+            require("nvim-treesitter.install").prefer_git = false
+            if vim.fn.has "win32" == 1 then
+                require("nvim-treesitter.install").compilers = { "clang", "cl", "zig" }
             else
-                require('nvim-treesitter.install').compilers = { 'gcc', 'clang', 'zig' }
+                require("nvim-treesitter.install").compilers = { "gcc", "clang", "zig" }
             end
-            require('nvim-treesitter.configs').setup {
-                ensure_installed = { 'python', 'c', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'vim', 'vimdoc', 'cpp' },
+            require("nvim-treesitter.configs").setup {
+                ensure_installed = { "python", "c", "html", "lua", "luadoc", "markdown", "markdown_inline", "vim", "vimdoc", "cpp" },
                 auto_install = true,
                 sync_install = false,
                 ignore_install = {},
                 modules = {},
                 highlight = {
                     enable = true,
-                    additional_vim_regex_highlighting = { 'ruby' },
+                    additional_vim_regex_highlighting = { "ruby" },
                 },
-                indent = { enable = true, disable = { 'ruby' } },
+                indent = { enable = true, disable = { "ruby" } },
             }
         end,
     },
     {
-        'nvim-treesitter/nvim-treesitter-context',
+        "nvim-treesitter/nvim-treesitter-context",
 
         opts = {
             enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
             max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
             min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
             line_numbers = true,
-            multiline_threshold = 20, -- Maximum number of lines to show for a single context
-            trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
-            mode = 'cursor', -- Line used to calculate context. Choices: 'cursor', 'topline'
+            multiline_threshold = 4, -- Maximum number of lines to show for a single context
+            trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+            mode = "cursor", -- Line used to calculate context. Choices: 'cursor', 'topline'
             -- Separator between context and content. Should be a single character string, like '-'.
             -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
             separator = nil,
